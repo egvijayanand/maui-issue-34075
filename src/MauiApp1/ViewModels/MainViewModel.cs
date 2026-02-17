54da70f719f9dfe30335713a7dc6b06feddc7c56
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2026-02-17 20:51:11 +0530

    Sample project to simulate the issue https://github.com/dotnet/maui/issues/34075

---
+++ src/MauiApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp1.ViewModels
+{
+    public partial class MainViewModel(ISemanticScreenReader screenReader) : BaseViewModel("MauiApp1")
+    {
+        private int _count;
+        private RelayCommand? incrementCommand;
+
+        public IRelayCommand IncrementCommand => incrementCommand ??= new RelayCommand(new Action(Increment));
+
+        [ObservableProperty]
+        public partial string CountText { get; private set; } = "Current count: 0";
+
+        //[RelayCommand] - Won't work as the generated Command won't be visible to .NET MAUI Source Gen
+        private void Increment()
+        {
+            _count++;
+            CountText = $"Current count: {_count}";
+            screenReader.Announce(CountText);
+        }
+    }
+}
